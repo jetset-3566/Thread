5505b5e0b0c7030ae8b7b5aa289fb0c2e2b32cf2
evgen chichilov <jetset.3566@gmail.ocm>
2021-05-01 17:40:53 +0600

    * Thread CPP part1 class method in thread

--- ThreadExampleCPP/Example.cpp
+++ ThreadExampleCPP/Example.cpp
@@ -11,52 +11,64 @@
 
 using namespace std;
 
-void SimpleAbstractWork(string tmp)
+class ThreadClass
 {
-	for (int i = 0; i < 10; i++)
+public:
+
+	void SimpleAbstractWork(string tmp)
 	{
-		cout << "Id Thread - " << std::this_thread::get_id() << "\t" << tmp <<  " DoWork -" << i << std::endl;
-		this_thread::sleep_for(std::chrono::milliseconds(1500));
+		for (int i = 0; i < 10; i++)
+		{
+			cout << "Id Thread - " << std::this_thread::get_id() << "\t" << tmp << " DoWork -" << i << std::endl;
+			this_thread::sleep_for(std::chrono::milliseconds(1000));
+		}
 	}
-}
 
-void LoopSimpleAbstractWork(string tmp)
-{
-	int i = 0;
-	while (true)
+	void LoopSimpleAbstractWork(string tmp)
 	{
-		cout << "Id Thread Loop - " << std::this_thread::get_id() << "\t" << tmp << " DoLoopWork -" << i << std::endl;
-		this_thread::sleep_for(std::chrono::milliseconds(1500));
-		i++;
+		int i = 0;
+		while (true)
+		{
+			cout << "Id Thread Loop - " << std::this_thread::get_id() << "\t" << tmp << " DoLoopWork -" << i << std::endl;
+			this_thread::sleep_for(std::chrono::milliseconds(1000));
+			i++;
+		}
 	}
-}
 
-int Sum(int a, int b)
-{
-	int result = -1;
-	this_thread::sleep_for(std::chrono::milliseconds(1000));
-	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Start---- "<< a << " + " << b <<" ---------------"<< endl;
+	int Sum(int a, int b)
+	{
+		int result = -1;
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Start---- " << a << " + " << b << " ---------------" << endl;
 
-	this_thread::sleep_for(std::chrono::milliseconds(3000));
+		this_thread::sleep_for(std::chrono::milliseconds(3000));
 
-	result = a+b;
+		result = a + b;
 
-	this_thread::sleep_for(std::chrono::milliseconds(1000));
-	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Done---- "<< "Result = "<< result <<" ---------------" << endl;
-	
-	return result;
-}
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Done---- " << "Result = " << result << " ---------------" << endl;
 
-void Assignment(int &a)
-{
-	this_thread::sleep_for(std::chrono::milliseconds(1000));
-	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Assignment Start---- " << a << " * 2 "<< " ----" << endl;
+		return result;
+	}
 
-	this_thread::sleep_for(std::chrono::milliseconds(3000));
+	void Assignment(int& a)
+	{
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Assignment Start---- " << a << " * 2 " << " ----" << endl;
 
-	a = a * 2;
-	cout << "---- "<<"Id Thread- " << std::this_thread::get_id() << "----Assignment Done---- " << "Result = "<< a << "  ----"<< endl;
-}
+		this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+		a = a * 2;
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Assignment Done---- " << "Result = " << a << "  ----" << endl;
+	}
+
+	void GreatJobWithoutParametr()
+	{
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----GreatJobWithoutParametr Start---- " << endl;
+		this_thread::sleep_for(std::chrono::milliseconds(4000));
+		cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----GreatJobWithoutParametr End---- " << endl;
+	}
+};
 
 int main()
 {
@@ -66,11 +78,38 @@ int main()
 
 	cout << "Main thread ID - "<< std::this_thread::get_id()<< endl;
 	
+	ThreadClass myThreadClass;
+	int Result;
+	int SecondResult = 5;
+
+	thread ThreadObjectWithLamda([&]()
+		{
+			Result = myThreadClass.Sum(5, 10);
+			myThreadClass.Assignment(SecondResult);
+		});
+
+	thread ThreadClassWithoutParametr([&]()
+		{
+			myThreadClass.GreatJobWithoutParametr();
+		});
+
+	thread ThreadClassWithoutParametrSecond(&ThreadClass::GreatJobWithoutParametr, myThreadClass);
+
+	myThreadClass.SimpleAbstractWork("Main");
+
+	ThreadObjectWithLamda.join();
+	ThreadClassWithoutParametr.join();
+	ThreadClassWithoutParametrSecond.join();
+
+	cout << Result << endl;
+	cout << SecondResult << endl;
+
+/*
 	thread JoinThread(SimpleAbstractWork, "JoinThread");
 	/*
 	* ждем, пока поток завершит свое выполнение	
 	* Любые выделенные ресурсы будут освобождены после выхода из потока.
-	*/
+	
 	cout << JoinThread.joinable() << endl;//Вывод булевой переменной проверяет не ассоциорван ли обьект(поток) ни с каким потоком
 	JoinThread.join();
 
@@ -79,7 +118,7 @@ int main()
 	* Отделяет поток выполнения от объекта потока(будь это main поток, или другой)
 	* позволяет потоку выполняться независимо от дескриптора(токен) потока
 	* Любые выделенные ресурсы будут освобождены после выхода из потока.
-	*/
+	
 	DetachThread.detach();
 
 	cout << DetachThread.joinable()<< endl;
@@ -90,7 +129,7 @@ int main()
 	/*Шаблоны функций ref и cref являются вспомогательными функциями
 	* которые генерируют объект типа std::reference_wrapper
 	* используя вывод аргументов шаблона для определения аргумента шаблона результата
-	*/
+	
 	thread AssignmentThread(Assignment,std::ref(GlobalVar));
 	cout<< GlobalVar<<endl;
 
@@ -106,5 +145,6 @@ int main()
 	SumThread.join();
 	
 	//LoopSimpleAbstractWork("Main");
+*/
 	return 0;
 }

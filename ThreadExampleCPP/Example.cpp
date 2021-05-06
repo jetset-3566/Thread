defbe59c44999de2b0320e811d4241d096aa3706
evgen chichilov <jetset.3566@gmail.ocm>
2021-05-06 22:27:40 +0600

    Thread CPP part2 Condition variable

--- ThreadExampleCPP/Example.cpp
+++ ThreadExampleCPP/Example.cpp
@@ -12,6 +12,9 @@
 #include <mutex>
 #include <ctime>  
 #include <vector>
+
+#include <condition_variable>
+#include <random>
 using namespace std;
 
 class ThreadClass
@@ -73,9 +76,64 @@ public:
 	}
 };
 
+void SimpleAbstractWork(string tmp)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Id Thread - " << std::this_thread::get_id() << "\t" << tmp << " DoWork -" << i << std::endl;
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+}
+void LoopSimpleAbstractWork(string tmp)
+{
+	int i = 0;
+	while (true)
+	{
+		cout << "Id Thread Loop - " << std::this_thread::get_id() << "\t" << tmp << " DoLoopWork -" << i << std::endl;
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+		i++;
+	}
+}
+int Sum(int a, int b)
+{
+	int result = -1;
+	this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Start---- " << a << " + " << b << " ---------------" << endl;
+
+	this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+	result = a + b;
+
+	this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Sum Done---- " << "Result = " << result << " ---------------" << endl;
+
+	return result;
+}
+void Assignment(int& a)
+{
+	this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Assignment Start---- " << a << " * 2 " << " ----" << endl;
+
+	this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+	a = a * 2;
+	cout << "---- " << "Id Thread- " << std::this_thread::get_id() << "----Assignment Done---- " << "Result = " << a << "  ----" << endl;
+}
+
 mutex myMutex1;
 mutex myMutex2;
 recursive_mutex myRecMutex;
+
+int GetRandomInRange(const int min, const int max)
+{
+	static std::default_random_engine gen(
+		static_cast<unsigned>(
+			std::chrono::system_clock::now().time_since_epoch().count()
+			)
+	);
+	std::uniform_int_distribution<int> distribution(min, max);
+	return distribution(gen);
+}
 
 void PrintVector(vector<int> VectorToPrint)
 {
@@ -94,7 +152,7 @@ void RandomFillVectorBySize(vector<int> &VectorToFill, int sizeVector)
 	VectorToFill.clear();
 	for (int i = 0; i < sizeVector; i++)
 	{
-		int randNumber = rand() % 100;
+		int randNumber = GetRandomInRange(0,100);
 		VectorToFill.push_back(randNumber);
 		printf("%d  ", randNumber);
 		this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -147,43 +205,133 @@ void RecMethod(int r)
 	myRecMutex.unlock();
 }
 
+condition_variable CV_FillVector;
+bool EndConsume = false;
+
+void Consume(int &value, vector<int> &Vector, int neededInt, mutex& locker)
+{
+	unique_lock<mutex> lock(locker);
+
+	bool bIsConsumeFill = false; 
+	while (!bIsConsumeFill)
+	{		
+		CV_FillVector.wait(lock);
+		if (value < 10)
+		{
+			unsigned int i = 0;
+			while(i < Vector.size())
+			{
+				if (Vector[i] == neededInt)
+				{
+					value++;
+					Vector.erase(Vector.begin() + i);
+					i--;
+					printf("Hey i found what i want - %d ", neededInt);	
+					printf("\n");
+					if (value > 10)
+					{
+						bIsConsumeFill = true;
+					}
+				}	
+				i++;
+			}
+			Vector.clear();		
+		}
+	}	
+	EndConsume = true;
+}
+
+void Produce(vector<int>& VectorToFill, mutex &locker)
+{
+	unique_lock<mutex> UL(locker, defer_lock);
+	
+	while (true)
+	{
+		this_thread::sleep_for(std::chrono::milliseconds(100));
+		int randNumber = GetRandomInRange(0, 10);
+
+		UL.lock();
+		VectorToFill.push_back(randNumber);
+		UL.unlock();
+		if (VectorToFill.size() > 50)
+		{
+			CV_FillVector.notify_one();			
+		}		
+	}
+}
+
 int main()
 {
-	chrono::time_point<chrono::steady_clock> StartClock, EndClock;
-	chrono::duration<float> DurationClock;
-	StartClock = chrono::high_resolution_clock::now();
-
-	srand(static_cast<unsigned int>(time(0)));
-	
 	vector<int> myVector;
+	vector<thread> threads;
 	
-	thread FillVectorTrhead1([&]()
-	{
-		FillVectorBySize(myVector, 55, 5);
-	});
+	int SuccessConsumeCout = 0;
 
-	thread FillVectorTrhead2([&]()
+	mutex m;
+	mutex &lockerFormyVector = m;
+
+	for (int i = 0; i < 2; i++)
 	{
-		RandomFillVectorBySize(myVector,5);
-	});
-	
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	this_thread::sleep_for(std::chrono::milliseconds(500));
-	//}
+		threads.push_back(thread([&]()
+		{
+			Produce(myVector, lockerFormyVector);
+		}));
+	}
+	for (thread& t : threads)
+	{
+		t.detach();
+	}
+	thread ConsumeThread(Consume, std::ref(SuccessConsumeCout), std::ref(myVector), 5, std::ref(lockerFormyVector));
+
+	while (!EndConsume)
+	{
+		printf("%d ", SuccessConsumeCout);
 		
-	FillVectorTrhead2.join();
-	FillVectorTrhead1.join();
+		this_thread::sleep_for(std::chrono::milliseconds(100));
+	}	
+	printf("\n");
+	printf("End work %d ", SuccessConsumeCout);
+	ConsumeThread.join();
+	
+//Mutex:
 
-	PrintVector(myVector);
+	//chrono::time_point<chrono::steady_clock> StartClock, EndClock;
+	//chrono::duration<float> DurationClock;
+	//StartClock = chrono::high_resolution_clock::now();
+	//
+	//srand(static_cast<unsigned int>(time(0)));
+	//
+	//vector<int> myVector;
+	//
+	//thread FillVectorTrhead1([&]()
+	//{
+	//	FillVectorBySize(myVector, 55, 5);
+	//});
+	//
+	//thread FillVectorTrhead2([&]()
+	//{
+	//	RandomFillVectorBySize(myVector,5);
+	//});
+	//
+	////for (int i = 0; i < 10; i++)
+	////{
+	////	this_thread::sleep_for(std::chrono::milliseconds(500));
+	////}
+	//	
+	//FillVectorTrhead2.join();
+	//FillVectorTrhead1.join();
+	//
+	//PrintVector(myVector);
+	//
+	////thread myThread(RecMethod, 3);
+	////myThread.join();
+	//
+	//EndClock = chrono::high_resolution_clock::now();
+	//DurationClock = EndClock - StartClock;
+	//printf("%f", DurationClock.count());
+	//system("pause");
 
-	//thread myThread(RecMethod, 3);
-	//myThread.join();
-
-	EndClock = chrono::high_resolution_clock::now();
-	DurationClock = EndClock - StartClock;
-	printf("%f", DurationClock.count());
-	system("pause");
+//input output Thread with class:
 
 /*
 	cout << "Core thread number - " << thread::hardware_concurrency() << endl;
@@ -218,47 +366,51 @@ int main()
 	cout << Result << endl;
 	cout << SecondResult << endl;
 */
-/*
-	thread JoinThread(SimpleAbstractWork, "JoinThread");
-	/*
-	* ждем, пока поток завершит свое выполнение	
-	* Любые выделенные ресурсы будут освобождены после выхода из потока.
-	
-	cout << JoinThread.joinable() << endl;//Вывод булевой переменной проверяет не ассоциорван ли обьект(поток) ни с каким потоком
-	JoinThread.join();
 
-	thread DetachThread(LoopSimpleAbstractWork, "DetachThread");
-	/*
-	* Отделяет поток выполнения от объекта потока(будь это main поток, или другой)
-	* позволяет потоку выполняться независимо от дескриптора(токен) потока
-	* Любые выделенные ресурсы будут освобождены после выхода из потока.
-	
-	DetachThread.detach();
+//Simple Thread:
 
-	cout << DetachThread.joinable()<< endl;
-	
-	//Assignment, доступ к переменной из потока по ссылки
-	int GlobalVar = 8;
-	cout << GlobalVar << endl;
-	/*Шаблоны функций ref и cref являются вспомогательными функциями
-	* которые генерируют объект типа std::reference_wrapper
-	* используя вывод аргументов шаблона для определения аргумента шаблона результата
-	
-	thread AssignmentThread(Assignment,std::ref(GlobalVar));
-	cout<< GlobalVar<<endl;
+	//thread JoinThread(SimpleAbstractWork, "JoinThread");
+	//
+	////ждем, пока поток завершит свое выполнение	
+	////Любые выделенные ресурсы будут освобождены после выхода из потока.
+	//
+	//cout << JoinThread.joinable() << endl;//Вывод булевой переменной проверяет не ассоциорван ли обьект(поток) ни с каким потоком
+	//JoinThread.join();
+	//
+	//thread DetachThread(LoopSimpleAbstractWork, "DetachThread");
+	//
+	////Отделяет поток выполнения от объекта потока(будь это main поток, или другой)
+	////позволяет потоку выполняться независимо от дескриптора(токен) потока
+	////Любые выделенные ресурсы будут освобождены после выхода из потока.
+	//
+	//DetachThread.detach();
+	//
+	//cout << DetachThread.joinable()<< endl;
+	//
+	////Assignment, доступ к переменной из потока по ссылки
+	//int GlobalVar = 8;
+	//cout << GlobalVar << endl;
+	//
+	////Шаблоны функций ref и cref являются вспомогательными функциями
+	////которые генерируют объект типа std::reference_wrapper
+	////используя вывод аргументов шаблона для определения аргумента шаблона результата
+	//
+	//thread AssignmentThread(Assignment,std::ref(GlobalVar));
+	//cout<< GlobalVar<<endl;
+	//
+	//int SumThreadResult;
+	////Sum доступ к выводу функции с помощью лямда-выражения
+	//thread SumThread([&SumThreadResult]() 
+	//{
+	//	SumThreadResult = Sum(4,6);
+	//});
+	//SimpleAbstractWork("Main");
+	//
+	//AssignmentThread.join();
+	//SumThread.join();
+	//
+	////LoopSimpleAbstractWork("Main");
 
-	int SumThreadResult;
-	//Sum доступ к выводу функции с помощью лямда-выражения
-	thread SumThread([&SumThreadResult]() 
-	{
-		SumThreadResult = Sum(4,6);
-	});
-	SimpleAbstractWork("Main");
-	
-	AssignmentThread.join();
-	SumThread.join();
-	
-	//LoopSimpleAbstractWork("Main");
-*/
+
 	return 0;
 }
